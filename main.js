4a533943248d939c8cb9566e8ab0317816865878
Bryant Richards <dragonseyeimages@gmail.com>
2020-08-24 08:38:11 -0500

    AXIOS INSTANCES

--- main.js
+++ main.js
@@ -166,6 +166,11 @@ axios.interceptors.request.use(
 );
 
 // AXIOS INSTANCES
+// const axiosInstance = axios.create({
+//   baseURL: 'https://jsonplaceholder.typicode.com'
+// })
+//
+// axiosInstance.get('/comments').then(res =>showOutput(res))
 
 // Show output in browser
 function showOutput(res) {
