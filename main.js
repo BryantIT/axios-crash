89ac6e67df789b991b9a7f2c94bdd98bbe1072ff
Bryant Richards <dragonseyeimages@gmail.com>
2020-08-24 07:28:13 -0500

    patch request

--- main.js
+++ main.js
@@ -31,7 +31,14 @@ function addTodo() {
 
 // PUT/PATCH REQUEST
 function updateTodo() {
-  console.log('PUT/PATCH Request');
+  axios.patch('https://jsonplaceholder.typicode.com/todos/1', {
+    data: {
+      title: 'Updated Todo',
+      completed: true
+    }
+  })
+  .then(res => showOutput(res))
+  .catch(err => console.log(err))
 }
 
 // DELETE REQUEST
